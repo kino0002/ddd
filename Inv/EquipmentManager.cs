6887493204dabfcf7482af68916c21e2205dbaed
kino <fitizenanon@protonmail.com>
2023-10-04 23:09:48 +0200

    dfd

---
+++ Inv/EquipmentManager.cs
@@ -0,0 +1,214 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EquipmentManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class EquipmentSlot
+    {
+        public EquipmentSlotType slotType;
+        public EquipmentDefinition equippedItem;
+        public GridItemContainer storageContainer;
+    }
+
+    [SerializeField]
+    private List<EquipmentSlot> equipmentSlots;
+
+    public event Action<EquipmentDefinition, EquipmentSlotType> OnEquipmentChanged;
+
+    private void Awake()
+    {
+        // Initialize equipmentSlots with default values
+        equipmentSlots = new List<EquipmentSlot>
+    {
+        new EquipmentSlot { slotType = EquipmentSlotType.PrimarySlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.HeadSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.ChestSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.LegsSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.BootsSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.SecondarySlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.NecklaceSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.BagSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.BeltSlot },
+        new EquipmentSlot { slotType = EquipmentSlotType.RingSlot }
+    };
+    }
+
+
+    public bool EquipItem(EquipmentDefinition newItem)
+    {
+        if (newItem == null)
+        {
+            return false;
+        }
+
+        EquipmentSlot slot = GetEquipmentSlot(newItem.Slot);
+
+        if (slot != null)
+        {
+            if (slot.equippedItem != null)
+            {
+                //Debug.Log($"Equipment slot {newItem.Slot} is already in use by {slot.equippedItem.itemName}.");
+                return false;
+            }
+
+            slot.equippedItem = newItem;
+            OnEquipmentChanged?.Invoke(newItem, slot.slotType);
+
+            if (newItem.MaxStorageSpace > 0)
+            {
+                slot.storageContainer = new GridItemContainer(newItem); // Updated this line
+            }
+
+            FindObjectOfType<InventoryUI>().UpdateStorageDisplay();
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void UnequipItem(EquipmentSlotType slotType)
+    {
+        EquipmentSlot slot = GetEquipmentSlot(slotType);
+        if (slot != null && slot.equippedItem != null)
+        {
+            EquipmentDefinition oldItem = slot.equippedItem;
+            slot.equippedItem = null;
+            OnEquipmentChanged?.Invoke(null, slotType);
+
+            if (oldItem.MaxStorageSpace > 0)
+            {
+                slot.storageContainer = null;
+                FindObjectOfType<InventoryUI>().UpdateStorageDisplay();
+            }
+        }
+    }
+
+    public EquipmentSlot GetEquipmentSlot(EquipmentSlotType slotType)  // Change from string to EquipmentSlotType
+    {
+        EquipmentSlot slot = equipmentSlots.Find(slot => slot.slotType == slotType);  // Remove ToString()
+
+        if (slot != null)
+        {
+            MessageDisplayManager.Instance.DisplayMessage("Equipment slot found for slotType: " + slotType);
+        }
+        else
+        {
+            Debug.LogWarning("Equipment slot not found for slotType: " + slotType);
+        }
+
+        return slot;
+    }
+
+
+    public List<EquipmentSlot> GetEquipmentSlots()
+    {
+        return equipmentSlots;
+    }
+
+    public void UnequipItemInstance(EquipmentDefinition equipmentInstance)
+    {
+        if (equipmentInstance == null)
+        {
+            Debug.LogError("equipmentInstance is null in UnequipItemInstance.");
+            return;
+        }
+
+        if (equipmentSlots == null || !equipmentSlots.Any())
+        {
+            Debug.LogError("equipmentSlots is either null or empty.");
+            return;
+        }
+
+        EquipmentSlot slot = equipmentSlots.Find(slot => slot.equippedItem == equipmentInstance);
+
+        if (slot == null)
+        {
+            Debug.LogError($"No slot found containing {equipmentInstance.itemName}.");
+            return;
+        }
+
+        Debug.Log("Unequipping item: " + equipmentInstance.itemName);
+        slot.equippedItem = null;
+        OnEquipmentChanged?.Invoke(slot.equippedItem, slot.slotType);
+
+        if (equipmentInstance.MaxStorageSpace > 0)
+        {
+            slot.storageContainer = null;
+        }
+
+        InventoryUI inventoryUI = FindObjectOfType<InventoryUI>();
+        if (inventoryUI != null)
+        {
+            inventoryUI.UpdateStorageDisplay();
+        }
+    }
+
+
+
+    public List<EquipmentSlot> GetStorageEquipments()
+    {
+        return equipmentSlots.FindAll(slot => slot.equippedItem != null && slot.equippedItem.MaxStorageSpace > 0);
+    }
+
+    public int GetTotalEquippedStorageSpace()
+    {
+        int totalSpace = 0;
+
+        foreach (var slot in equipmentSlots)
+        {
+            if (slot.equippedItem != null && slot.equippedItem.MaxStorageSpace > 0)
+            {
+                totalSpace += slot.equippedItem.MaxStorageSpace;
+            }
+        }
+
+        return totalSpace;
+    }
+
+    public void RemoveItem(EquipmentDefinition itemToRemove)
+    {
+        if (itemToRemove == null)
+        {
+            Debug.LogWarning("Tried to remove a null item.");
+            return;
+        }
+
+        // Find the slot containing the item to be removed
+        EquipmentSlot slot = equipmentSlots.Find(s => s.equippedItem == itemToRemove);
+
+        if (slot != null)
+        {
+            // Log the initial states
+            Debug.Log($"Before removal: Slot type: {slot.slotType}, Equipped item: {slot.equippedItem?.itemName}, Storage container: {slot.storageContainer}");
+
+            // Remove the item from the slot
+            slot.equippedItem = null;
+
+            // If the item had storage space, clear the storage container
+            if (itemToRemove.MaxStorageSpace > 0)
+            {
+                slot.storageContainer = null;
+            }
+
+            // Log the final states
+            Debug.Log($"After removal: Slot type: {slot.slotType}, Equipped item: {slot.equippedItem}, Storage container: {slot.storageContainer}");
+
+            // Update the inventory UI
+            InventoryUI inventoryUI = FindObjectOfType<InventoryUI>();
+            if (inventoryUI != null)
+            {
+                inventoryUI.UpdateStorageDisplay();
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"Item {itemToRemove.itemName} not found in any slot.");
+        }
+    }
+
+}
---
+++ Inv/ItemPickup.cs
@@ -0,0 +1,139 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item item;
+    public float pickupDelay = 0.5f;
+    private float timeSinceThrown;
+    public int playerLayer = 8;
+    private InventoryUI inventoryUIInstance;
+
+    public List<ItemStack> StoredItems { get; set; }
+
+    public void SetTimeSinceThrown(float time)
+    {
+        timeSinceThrown = time;
+    }
+
+    private void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null && item != null)
+        {
+            spriteRenderer.sprite = item.Icon;
+            UpdateColliderShape();
+        }
+        playerLayer = LayerMask.NameToLayer("Player");
+        inventoryUIInstance = FindObjectOfType<InventoryUI>();
+    }
+
+    private void UpdateColliderShape()
+    {
+        PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
+        if (polygonCollider != null)
+        {
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+            if (sprite != null)
+            {
+                polygonCollider.pathCount = sprite.GetPhysicsShapeCount();
+                List<Vector2> path = new List<Vector2>();
+
+                for (int i = 0; i < polygonCollider.pathCount; i++)
+                {
+                    path.Clear();
+                    sprite.GetPhysicsShape(i, path);
+                    polygonCollider.SetPath(i, path);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Time.time > timeSinceThrown + pickupDelay)
+        {
+            EquipmentManager equipmentManager = other.GetComponent<EquipmentManager>();
+            if (equipmentManager == null) return;
+
+            if (item is EquipmentDefinition equipment)
+            {
+                HandleEquipmentPickup(equipmentManager, equipment);
+            }
+            else
+            {
+                HandleItemPickup(equipmentManager);
+            }
+        }
+    }
+
+    private void HandleEquipmentPickup(EquipmentManager equipmentManager, EquipmentDefinition equipment)
+    {
+        if (equipment == null)
+        {
+            Debug.LogError("Equipment is null. Cannot equip.");
+            return;
+        }
+
+        EquipmentManager.EquipmentSlot slot = equipmentManager.GetEquipmentSlot(equipment.Slot);
+
+        if (slot == null)
+        {
+            Debug.LogError($"No slot found for type {equipment.Slot}. Cannot equip.");
+            return;
+        }
+
+        if (slot.equippedItem == null)
+        {
+            if (equipment is IEquippable equippableItem)
+            {
+                equippableItem.Equip(equipmentManager);
+
+                if (StoredItems != null && StoredItems.Count > 0 && slot.storageContainer != null)
+                {
+                    foreach (var itemStack in StoredItems)
+                    {
+                        slot.storageContainer.AddItem(itemStack.Item); // Fix here
+                    }
+                }
+                Destroy(gameObject);
+                inventoryUIInstance?.UpdateStorageDisplay();
+            }
+        }
+        else
+        {
+            string occupyingItemName = slot.equippedItem.itemName;
+        }
+    }
+
+    private void HandleItemPickup(EquipmentManager equipmentManager)
+    {
+        bool itemAdded = false;
+
+        // Loop through all storage slots and attempt to add the item
+        foreach (var addSlot in equipmentManager.GetEquipmentSlots())
+        {
+            if (addSlot.equippedItem != null && addSlot.equippedItem.MaxStorageSpace > 0 && addSlot.storageContainer != null)
+            {
+                if (addSlot.storageContainer.AddItem(item)) // Assuming AddItem returns true if item was added
+                {
+                    itemAdded = true;
+                    Destroy(gameObject);
+                    inventoryUIInstance?.UpdateStorageDisplay();
+                    break;  // Break out of the loop if the item was added
+                }
+                else
+                {
+                    Debug.Log($"Failed to add item to {addSlot.equippedItem.itemName}");
+                }
+            }
+        }
+
+        if (!itemAdded)
+        {
+            Debug.Log("No storage container found to pick up the item.");
+        }
+    }
+
+
+}
